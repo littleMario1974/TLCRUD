aadc1bdca59e79269be69f0cb5eeb62dd8cb1c2d
littleMario1974 <91436477+littleMario1974@users.noreply.github.com>
2023-01-13 20:19:27 +0100

    TLCRUD

--- src/main/java/com/example/tlcrud/user/User.java
+++ src/main/java/com/example/tlcrud/user/User.java
@@ -1,10 +1,10 @@
 package com.example.tlcrud.user;
 
 public class User {
-    Long id;
-    String username;
-    String email;
-    Boolean enabled;
+    public Long id;
+    public String username;
+    public String email;
+    public Boolean enabled;
 
     public User(Long id) {
         this.id = id;
